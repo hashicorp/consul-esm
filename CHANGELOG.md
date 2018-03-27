4704c795101e306dfc355ed29affba72cc9e04b6
Kyle Havlovitz <kylehav@gmail.com>
2018-03-27 13:06:08 -0700

    Update the tree to v0.3.0-dev mode

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 IMPROVEMENTS:
 
+BUG FIXES:
+
+## v0.2.0 (March 27, 2018)
+
+IMPROVEMENTS:
+
   * Correctly exit on SIGTERM as well as SIGINT.
   * Use a simpler UDP ping that doesn't require root privileges. [GH-5]
   * Add a config option for setting the ping method (UDP or socket).
--- version/version.go
+++ version/version.go
@@ -14,19 +14,19 @@ var (
 	GitDescribe string
 
 	// The main version number that is being run at the moment.
-	Version = "0.2.0"
+	Version = "0.3.0"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
+	VersionPrerelease = "dev"
 )
 
 // GetHumanVersion composes the parts of the version in a way that's suitable
 // for displaying to humans.
 func GetHumanVersion() string {
 	version := Version
-	if GitDescribe != "" {
+	if GitDescribe != "" && VersionPrerelease == "" {
 		version = GitDescribe
 	}
 
