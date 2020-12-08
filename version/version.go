05decac85e96b3d5df768206cb3e8c68984065d0
Lorna Song <lorna@hashicorp.com>
2020-12-07 20:54:48 -0500

    Fix versioning bugs
     - Instead of overwriting the version variable, the variable value should be changed
     - Release process was pattern-matching on the pattern described in the comment instead of the actual version

--- version/version.go
+++ version/version.go
@@ -21,8 +21,7 @@ var (
 	GitDescribe string
 
 	// Version is the main version number that is being run at the moment.
-	// Note: our current release process searches this file for "Version ="
-	Version = "0.4.0"
+	// Note: our current release process does a pattern match on this variable.
 	Version = "0.5.0"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is ""
