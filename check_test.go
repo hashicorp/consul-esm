cb04a16c9197c17ddc8d7766125020d26b27abbc
André Cruz <acruz@cloudflare.com>
2021-01-19 17:12:46 +0000

    Added comment in tests

--- check_test.go
+++ check_test.go
@@ -402,6 +402,9 @@ func TestCheck_NoFlapping(t *testing.T) {
 	assert.Equal(t, 0, originalCheck.successCounter)
 	assert.Equal(t, api.HealthCritical, originalCheck.Status)
 
+	// test if counter values are kept after calling
+	// UpdateChecks(), which is called everytime there is
+	// a change in the consul catalog
 	runner.UpdateCheck(id, api.HealthPassing, "")
 	assert.Equal(t, 0, originalCheck.failureCounter)
 	assert.Equal(t, 1, originalCheck.successCounter)
