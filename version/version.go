29b9259de97798e334d5119c1baf5cb8ac5ba023
Sai Shashank Nandigama <saishashank.nandigama@hashicorp.com>
2025-09-19 15:34:34 +0530

    update changelog and version for 0.9.1 release

--- version/version.go
+++ version/version.go
@@ -25,7 +25,7 @@ var (
 
 	// Version is the main version number that is being run at the moment.
 	// Note: our current release process does a pattern match on this variable.
-	Version = "0.9.0"
+	Version = "0.9.1"
 
 	// VersionPrerelease is a pre-release marker for the version. If this is ""
 	// (empty string) then it means that it is a final release. Otherwise, this
